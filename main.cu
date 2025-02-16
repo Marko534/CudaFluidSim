c54280e05ed940071f0a1231f888e145a554e945
mare <markoilioski21@gmail.com>
2025-02-16 03:39:06 +0100

    Added init vectors.

--- main.cu
+++ main.cu
@@ -2,6 +2,7 @@
 #include <cuda_runtime.h>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+#include <glm/glm.hpp>
 
 cudaError_t ercall;
 #define CCALL(call)                                                                                                    \
@@ -53,6 +54,74 @@ __device__ bool barrier[grid_l * grid_h];
 
 #define cellXFromPos(p) (int)p.x
 #define cellYFromPos(p) (int)p.y
+
+inline __device__ void init_vec()
+{
+    const int id = threadIdx.x + blockIdx.x * blockDim.x;
+    const int x = id % grid_l;
+    const int y = id / grid_l;
+
+    horizontalVectors[rightVecIndex(x, y)] = glm::vec2(0.0f, 0.0f);
+    verticalVectors[upVecIndex(x, y)] = glm::vec2(0.0f, 0.0f);
+    if (y == grid_h - 1)
+    {
+        verticalVectors[downVecIndex(x, y)] = glm::vec2(0.0f, 0.0f);
+    }
+    if (x == 0)
+    {
+        horizontalVectors[leftVecIndex(x, y)] = glm::vec2(0.0f, 0.0f);
+    }
+}
+
+inline __device__ void init_vecBuffer()
+{
+    const int id = threadIdx.x + blockIdx.x * blockDim.x;
+    const int x = id % grid_l;
+    const int y = id / grid_l;
+
+    horizontalVectorsBuffer[rightVecIndex(x, y)] = glm::vec2(0.0f, 0.0f);
+    verticalVectorsBuffer[upVecIndex(x, y)] = glm::vec2(0.0f, 0.0f);
+    if (y == grid_h - 1)
+    {
+        verticalVectorsBuffer[downVecIndex(x, y)] = glm::vec2(0.0f, 0.0f);
+    }
+    if (x == 0)
+    {
+        horizontalVectorsBuffer[leftVecIndex(x, y)] = glm::vec2(0.0f, 0.0f);
+    }
+}
+
+inline __device__ void set_horizontal_vec_cell(const glm::vec2 v, const int x, const int y)
+{
+    horizontalVectors[rightVecIndex(x, y)] = v;
+    horizontalVectors[leftVecIndex(x, y)] = v;
+}
+
+inline __device__ void set_vertical_vec_cell(const glm::vec2 v, const int x, const int y)
+{
+    verticalVectors[upVecIndex(x, y)] = v;
+    verticalVectors[downVecIndex(x, y)] = v;
+}
+
+__global__ void setHorizontalVec(const glm::vec2 v, const int x, const int y)
+{
+    set_horizontal_vec_cell(v, x, y);
+}
+
+__global__ void setVerticalVec(const glm::vec2 v, const int x, const int y)
+{
+    set_vertical_vec_cell(v, x, y);
+}
+
+void setHorVecs(const glm::vec2 v, const int x, const int y)
+{
+    setHorizontalVec<<<1, 1>>>(v, x, y);
+}
+
+void setVertVecs(const glm::vec2 v, const int x, const int y)
+{
+    setVerticalVec<<<1, 1>>>(v, x, y);
+}
 
 int main()
 {
